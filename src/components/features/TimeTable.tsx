0995b910fd2da42e015b827a03b11b324b8e202f
LaCall-Gagans-Studio <studio@lacall-gagans.com>
2026-09-06 23:41:26 +0900

    pwc 2

--- src/components/features/TimeTable.tsx
+++ src/components/features/TimeTable.tsx
@@ -66,7 +66,7 @@ const timetableData: SessionItem[] = [
         time: "",
         text: "地域活性化人材（西会津町・萩市）",
         desc: "「未来型”結”のポテンシャル」",
-        speakers: "野崎涼（PwCコンサルティング合同会社）",
+        speakers: "野﨑 涼（PwCコンサルティング合同会社）",
         sub: true,
       },
       {
@@ -104,7 +104,7 @@ const timetableData: SessionItem[] = [
       {
         time: "",
         text: "放課後共創基金",
-        speakers: "小玉 祥平（三豊市教育センター長） × 篠崎亮（PwCコンサルティング合同会社）",
+        speakers: "小玉 祥平（三豊市教育センター長） × 篠崎 亮（PwCコンサルティング合同会社）",
         sub: true,
       },
       {
